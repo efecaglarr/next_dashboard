f67d26170f700e905b7cdd2418146354d55360f5
efe <efecagglar@gmail.com>
2025-02-26 23:38:46 -0500

    Remove .dist directory and update .gitignore

--- server/dist/config/google.config.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.googleClient = void 0;
-const google_auth_library_1 = require("google-auth-library");
-exports.googleClient = new google_auth_library_1.OAuth2Client(process.env.GOOGLE_CLIENT_ID, process.env.GOOGLE_CLIENT_SECRET, `${process.env.SERVER_URL}/auth/google/callback`);
